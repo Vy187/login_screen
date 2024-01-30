3b3d98ae299ba35d1855c649bdc4a6afabc453cc
Vy Dinh Nguyen <vynguyen@Vys-MacBook-Pro.local>
2024-01-29 17:32:12 -0800

    build: installed basic dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  return (
+    <>
+    <main>hello</main>
+    </>
+  )
+}
+
+export default App
